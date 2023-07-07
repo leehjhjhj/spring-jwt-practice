e61cea9a9ac7b75b049336a7cd27361d223ed4c6
leehjhjhj <tsukiakarii@naver.com>
2023-07-08 00:57:44 +0900

    feat:complete access token

---
+++ src/main/java/com/imasimdi/jwt/sign/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.imasimdi.jwt.sign;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Transactional
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByAccount(String account);
+}
---
+++ src/test/java/com/imasimdi/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imasimdi.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
